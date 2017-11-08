807f0da13c7fd33f165e459e3cfac39946c9d57e
Jugador <dm.evilart@gmail.com>
2017-11-08 13:22:53 +0100

    Refactorizado el codigo

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,4 +1,3 @@
-
 /**
  * This basic calendar return the date (dd-mm-yy), 
  * allow to change the date by 
@@ -10,47 +9,42 @@
 public class CalendarioBasico
 {
     // Save the dia.
-    private int dia;
+    private DisplayDosCaracteres dia;
     // Save the mes.
-    private int mes;
+    private DisplayDosCaracteres mes;
     // Save the ano.
-    private int ano;
+    private DisplayDosCaracteres ano;
     
     /**
      * Constructor for objects of class CalendarioBasico.
      */
     public CalendarioBasico()
     {
-        dia = 1;
-        mes = 1;
-        // There is no world before 2000.
-        // The Eschaton comes in 2100.
-        ano = 1;
+        dia = new DisplayDosCaracteres (31);
+        mes = new DisplayDosCaracteres (13);
+        ano = new DisplayDosCaracteres (100);
     }
 
     /**
      * Returns date in format (dd-mm-yy) with 8 characters. 
-     */
+    */
     public String obtenerFecha ()
     {
         // Create a text chain like: '1dd-1mm-1yy'.
-        String date = (dia+100) + "-" + (mes+100) + "-" + (ano+100);
-        // Now, ignore '1' and take the rest.
-        date = date.substring(1,4) 
-               + date.substring(5,8) 
-               + date.substring(9,11);
-        return date;
+        return dia.getTextoDelDisplay() + "-" 
+            + mes.getTextoDelDisplay() + "-" 
+            + ano.getTextoDelDisplay();
     }
     
     /**
      * Set a specific date.
      * Note: There are no control methods to validate data entry.
      */
-    public void fijarFecha (int setDia, int setMes, int setAno) 
+    public void fijarFecha (int setDay, int setMonth, int setYear) 
     {
-        dia = setDia;
-        mes = setMes;
-        ano = setAno;
+        dia.setValorAlmacenado(setDay);
+        mes.setValorAlmacenado(setMonth);
+        ano.setValorAlmacenado(setYear);
     }    
     /**
      * Add one dia to the date.
@@ -59,33 +53,13 @@ public class CalendarioBasico
      */
     public void avanzarFecha ()
     {
-        // Check if adding a dia changes the mes or ano.
-        if (dia == 30)
-        {
-            dia = 01;
-            if (mes == 12) 
-            {
-                mes = 01;
-                ano += 1;                
+        // Check if adding a dia changes the mes or ano. 
+            if (dia.getValorAlmacenado() == 30) {
+                 mes.incrementaValorAlmacenado();
+                    if (mes.getValorAlmacenado() == 1){
+                        ano.incrementaValorAlmacenado();
+                    }
             }
-            else 
-            {
-                mes += 1;
-            }
-        }
-        // 'Cristian hates me' validation. 
-        // Stop the Eschaton, 2100 never comes!
-        // If it is going to reach 2100, 
-        // set calendar to default.
-        else if (dia == 30 && mes == 12 && ano == 99)
-        {
-            dia = 1;
-            mes = 1;
-            ano = 0;
-        }
-        else 
-        {
-            dia += 1;
-        }
-    }    
-}
+            dia.incrementaValorAlmacenado();
+    }
+}    
